be722660379cf9e71ab98134a04ae21b88640eca
Joshua Wilkinson <josh@Joshuas-MacBook-Pro.local>
2023-02-14 06:14:37 +0000

    modify app.jsx and add components files

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,29 @@ import styles from "./style";
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX}${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}
-
-        
+        <div className={`${styles.boxWidth}`}>
+        Navbar
       </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        HERO
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        Stats
+        Business
+        Billing
+        CarDeal
+        Testimonials
+        Clients
+        CTA
+        Footer
+      </div>
+    </div>
+
     </div>
   ); 
 
